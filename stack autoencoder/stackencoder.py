5fc1b90a32a514db77a3d6df1b11bf8ad1605f8f
Fdslk <907081174@qq.com>
2019-08-01 10:23:11 +0800

    add Gaussian Noise

--- stack autoencoder/stackencoder.py
+++ stack autoencoder/stackencoder.py
@@ -1,54 +1,79 @@
 import numpy as np
-np.random.seed(1337)  # for reproducibility
- 
-from keras.datasets import mnist
-from keras.models import Model #泛型模型
-from keras.layers import Dense, Input
-import matplotlib.pyplot as plt
- 
-# X shape (60,000 28x28), y shape (10,000, )
-(x_train, _), (x_test, y_test) = mnist.load_data()
- 
-# 数据预处理
-x_train = x_train.astype('float32') / 255. - 0.5       # minmax_normalized
-x_test = x_test.astype('float32') / 255. - 0.5         # minmax_normalized
-x_train = x_train.reshape((x_train.shape[0], -1))
-x_test = x_test.reshape((x_test.shape[0], -1))
-print(x_train.shape)
-print(x_test.shape)
- 
-# 压缩特征维度至2维
-encoding_dim = 2
- 
-# this is our input placeholder
-input_img = Input(shape=(784,))
- 
-# 编码层
-encoded = Dense(128, activation='relu')(input_img)
-encoded = Dense(64, activation='relu')(encoded)
-encoded = Dense(10, activation='relu')(encoded)
-encoder_output = Dense(encoding_dim)(encoded)
- 
-# 解码层
-decoded = Dense(10, activation='relu')(encoder_output)
-decoded = Dense(64, activation='relu')(decoded)
-decoded = Dense(128, activation='relu')(decoded)
-decoded = Dense(784, activation='tanh')(decoded)
- 
-# 构建自编码模型
-autoencoder = Model(inputs=input_img, outputs=decoded)
- 
-# 构建编码模型
-encoder = Model(inputs=input_img, outputs=encoder_output)
- 
-# compile autoencoder
-autoencoder.compile(optimizer='adam', loss='mse')
- 
-# training
-autoencoder.fit(x_train, x_train, epochs=20, batch_size=256, shuffle=True)
- 
-# plotting
-encoded_imgs = encoder.predict(x_test)
-plt.scatter(encoded_imgs[:, 0], encoded_imgs[:, 1], c=y_test, s=3)
-plt.colorbar()
-plt.show()
+import sklearn.preprocessing as prep
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+def xavier_init(fan_in, fan_out, constant = 1):
+    low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
+    high = constant * np.sqrt(6.0 / (fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out), minval = low, maxval = high, dtype=tf.float32)
+
+class AdditiveGaussianNoiseAutoencoder(object):
+    def __init__(self, n_input, n_hidden, transfer_function = tf.nn.softplus, optimizer = tf.train.AdamOptimizer(), scale = 0.1):
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.transfer = transfer_function
+        self.scale = tf.placeholder(tf.float32)
+        self.training_scale = scale
+        network_weights = self._initialize_weights()
+        self.weights = network_weights
+        self.x = tf.placeholder(tf.float32, [None, self.n_input])
+        self.hidden = self.transfer(tf.add(tf.matmul(self.x + scale * tf.random_normal((n_input, )), self.weights['w1']), self.weights['b1']))
+        self.reconstruction = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
+        self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
+        self.optimizer = optimizer.minimize(self.cost)
+        
+        init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(init)
+    
+    def _initialize_weights(self):
+        all_weights = dict()
+        all_weights['w1'] = tf.Variable(xavier_init(self.n_input, self.n_hidden))
+        all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden], dtype = tf.float32))
+        all_weights['w2'] = tf.Variable(tf.zeros([self.n_hidden, self.n_input], dtype=tf.float32))
+        all_weights['b2'] = tf.Variable(tf.zeros([self.n_input], dtype = tf.float32))
+        return all_weights
+    
+    def partial_fit(self, X):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X, self.scale: self.training_scale})
+        return cost
+    
+    def calc_total_cost(self, X):
+        return self.sess.run(self.cost, feed_dict={self.x: X, self.scale: self.training_scale})
+    
+    def transform(self, X):
+        return self.sess.run(self.cl, feed_dict={self.x: X, self.scale: self.training_scale})
+    
+    def generate(self, hidden=None):
+        if hidden is None:
+            hidden = np.random.normal(size = self.weights["n1"])
+        return self.sess.run(self.reconstruction, feed_dict={self.hidden: hidden})
+    
+    def reconstruct(self, X):
+        return self.sess.run(self.reconstruction, feed_dict={self.x: X, self.scale: self.training_scale})
+    
+    def getWeights(self, X):
+        return self.sess.run(self.weights['w1'])
+    
+    def getBiases(self):
+        return self.sess.run(self.weights['b1'])
+    
+mnist =input_data.read_data_sets('MNIST_data', one_hot=True)
+def standard_scale(x_train, x_test):
+    preprocessor= prep.StandardScaler().fit(x_train)
+    x_train = preprocessor.transform(x_train)
+    x_test = preprocessor.transform(x_test)
+    return x_train, x_test
+
+def get_random_block_from_data(data, batch_size):
+    start_index = np.random.randint(0, len(data) - batch_size)
+    return data[start_index:(start_index + batch_size)]
+
+x_train, x_test = standard_scale(mnist.train.images, mnist.test.images)
+n_samples = int(mnist.train.num_examples)
+training_epochs = 20
+batch_size = 128
+display_step = 1
+
+autoencoder = AdditiveGaussianNoiseAutoencoder(n_input=784, n_hidden=200, transfer_function=tf.nn.softplus, optimizer=tf.train.AdamOptimizer(learning_rate=0.001), scale=0.01)
